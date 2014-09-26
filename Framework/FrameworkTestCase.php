9872a3da0091d8b0946acc697ab30762621da769
Babacooll <michael.garrez@gmail.com>
2014-09-26 15:56:19 +0100

    First commit

---
+++ Framework/FrameworkTestCase.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace DJ\MailingBundle\Test;
+
+/**
+ * Extended Framework Test Case created to implement easily Faker into the PHPUnit tests.
+ * Implements getMethod method to access private/protected methods from classes
+ *
+ * Class DJFrameworkTestCase
+ *
+ * @package DJ\MailingBundle\Test
+ */
+class DJFrameworkTestCase extends \PHPUnit_Framework_TestCase
+{
+    /** @var \Faker\Generator $faker */
+    protected $faker;
+
+    /**
+     * Calls PHPUnit_Framework_TestCase
+     * Creates a Faker instance
+     */
+    public function __construct()
+    {
+        parent::__construct();
+
+        $this->faker = \Faker\Factory::create();
+    }
+
+    /**
+     * Returns a method from a defined class whatever its visibility is
+     *
+     * @param $className
+     * @param $methodName
+     *
+     * @return \ReflectionMethod
+     */
+    public function getMethod($className, $methodName)
+    {
+        $class  = new \ReflectionClass($className);
+        $method = $class->getMethod($methodName);
+        $method->setAccessible(true);
+
+        return $method;
+    }
+}
